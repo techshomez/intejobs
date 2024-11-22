611489375f8dd36f0163ceee5ccd5c508345fed5
techshomez <danieltopeg@gmail.com>
2024-11-22 12:07:59 +0000

    navigation menu responsiveness

--- app/components/common/header/index.js
+++ app/components/common/header/index.js
@@ -2,6 +2,8 @@
 import './header.css';
 import Image from 'next/image';
 import Link from 'next/link'
+import { Menu__list } from '../../functions/menuData';
+import {HiOutlineUser, HiOutlineMenu } from 'react-icons/hi'
 
 function Header() {
   return (
@@ -11,16 +13,18 @@ function Header() {
         <Image src='/images/logo.png' width={230} height={44} alt='' />
         </a>
         <div className="navbar">
-            <Link href='/'>Home</Link>
-            <Link href='/'>Home</Link>
-            <Link href='/'>Home</Link>
-            <Link href='/'>Home</Link>
-            <Link href='/'>Home</Link>
-            <Link href='/'>Home</Link>
+            {Menu__list.map((m__item, index)=>{
+            return(
+               <Link href={m__item.path} key={index}>{m__item.title}</Link>
+            );
+        })}
         </div>
         <div className="cta">
             <Link href='/'>Login</Link>
-            <Link href='/'>Signup</Link>
+            <Link href='/'><i><HiOutlineUser /></i>Signup</Link>
+        </div>
+        <div className="mobile">
+            <i><HiOutlineMenu /></i>
         </div>
     </div>
     </>
---
+++ app/components/functions/menuData.js
@@ -0,0 +1,27 @@
+export const Menu__list =[
+    {
+        title:'Home',
+        path:'/'
+    },
+
+    {
+        title:'About Us',
+        path:'/about'
+    },
+    {
+        title:'For Job Seekers',
+        path:'/for-job-seekers'
+    },
+    {
+        title:'For Employers',
+        path:'/for-employers'
+    },
+    {
+        title:'For Reentry',
+        path:'/for-reentry'
+    },
+    {
+        title:'Contact Us',
+        path:'/contact-us'
+    },
+];
