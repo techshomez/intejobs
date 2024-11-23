bd71b473deaf38caae09468ba3d05711ecdc9d86
techshomez <danieltopeg@gmail.com>
2024-11-23 21:03:46 +0000

    mobile menu

--- app/components/common/header/index.js
+++ app/components/common/header/index.js
@@ -3,9 +3,22 @@ import './header.css';
 import Image from 'next/image';
 import Link from 'next/link'
 import { Menu__list } from '../../functions/menuData';
-import {HiOutlineUser, HiOutlineMenu } from 'react-icons/hi'
+import {HiOutlineUser, HiOutlineMenu, HiOutlineX} from 'react-icons/hi';
+import { useState } from 'react';
+
 
 function Header() {
+
+    const [click, setClick]=useState(false);
+    const [mobileToggle, setMobileToggle]=useState(false);
+
+    const toggleMenu = () => {
+        setClick(!click);
+        setMobileToggle(!mobileToggle);
+      };
+
+
+
   return (
     <>
     <div className="header">
@@ -23,9 +36,19 @@ function Header() {
             <Link href='/'>Login</Link>
             <Link href='/'><i><HiOutlineUser /></i>Signup</Link>
         </div>
-        <div className="mobile">
-            <i><HiOutlineMenu /></i>
+        <div className={click ? "mobile__close" : "mobile__button"} onClick={toggleMenu}>
+            <i>{click?<HiOutlineX />:<HiOutlineMenu />}</i>
         </div>
+
+        {mobileToggle && (
+            <div className="mobile__nav">
+                {Menu__list.map((m__item, index)=>{
+                return(
+                   <Link href={m__item.path} key={index}>{m__item.title}</Link>
+                );
+            })}
+            </div>
+        )}
     </div>
     </>
   )
